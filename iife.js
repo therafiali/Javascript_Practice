fac7fc2afbc072e6b43ddec315b073fccadd223c
Muhammad Rafi Ali <therafiali@gmail.com>
2024-04-04 20:49:26 +0500

    iife js

--- iife.js
+++ iife.js
@@ -1,10 +1,22 @@
 // Immediate invoke function expression
 
 //function run automatically no need to call the function
-(function() {
+// named IIFE
+(function run() {
     console.log("IIFE Ran..");
 })();
+
+// output:  IIFE Ran..
 
 (()=> {
     console.log("this is arrow function with iife");
 })();
+
+// output:  this is arrow function with iife
+
+((name)=> {
+    console.log("Welcome " + name);
+})("Rafi");
+
+
+// output:  Welcome Rafi
